38f48ec04caff5b648e3cf52c3b78c645dcc7b76
G.GMAPS <quannguyenn25111@gmail.com>
2026-09-16 22:17:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// CHỈ CẦN SỬA DÒNG APP_BASE_URL.
+// Dán link Web App Google Apps Script dạng:
+// https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+//
+// Wrapper này LUÔN tự thêm ?admin=1.
+// Không cần dán ?admin=1 vào đây.
+
+window.ADMIN_PWA_CONFIG = {
+  APP_BASE_URL: 'DAN_LINK_WEB_APP_APPS_SCRIPT_EXEC_VAO_DAY'
+};
