0700f2c61552b5c564fc22fb1b6b44c302b09d18
G.GMAPS <quannguyenn25111@gmail.com>
2026-09-16 22:23:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 // Không cần dán ?admin=1 vào đây.
 
 window.ADMIN_PWA_CONFIG = {
-  APP_BASE_URL: 'DAN_LINK_WEB_APP_APPS_SCRIPT_EXEC_VAO_DAY'
+  APP_BASE_URL: 'https://script.google.com/macros/s/AKfycbz2c3Ud95ElKmXTnj7sY9O_m_llFzB8Qdt1wiIOnZCXa7IeOAS7eAMLTFuvo_-XxbPU/exec'
 };
